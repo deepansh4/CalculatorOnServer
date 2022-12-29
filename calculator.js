c5d4554fac4eb1580566372456bd04e2cb8cacd1
Deepansh Kumar <92592259+deepansh4@users.noreply.github.com>
2022-12-29 22:23:31 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
-const bodyParser = require("body-parser");
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
